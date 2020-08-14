66e27f67e4a4b9c29813ab5f758cccb230e656eb
Justin Miller <justinrmiller@gmail.com>
2020-08-13 18:06:25 -0700

    Mostly cleanup, extracting out functions, etc but also adding a count endpoint to return the number of weather entries in Cassandra.

--- faust-cassandra-weather-example/app.py
+++ faust-cassandra-weather-example/app.py
@@ -9,11 +9,11 @@ cluster = Cluster()
 
 cassandra_session = cluster.connect()
 
+# wrap the Cassandra Session with aiocassandra
 aiosession(cassandra_session)
 
 app = faust.App(
     id="weather-app",
-    debug=settings.DEBUG,
     autodiscover=["weather"],
     broker=settings.KAFKA_BOOTSTRAP_SERVER,
     store=settings.STORE_URI,
@@ -22,3 +22,9 @@ app = faust.App(
     topic_disable_leader=settings.TOPIC_DISABLE_LEADER,
     broker_credentials=settings.SSL_CONTEXT,
 )
+
+# adds kafka and other stats to http://localhost:6066/stats/
+app.web.blueprints.add('/stats/', 'faust.web.apps.stats:blueprint')
+
+if __name__ == "__main__":
+    app.main()
--- faust-cassandra-weather-example/weather/agents.py
+++ faust-cassandra-weather-example/weather/agents.py
@@ -8,7 +8,12 @@ import uuid
 
 from faust import current_event
 
+from cassandra import ConsistencyLevel
+
+from cassandra.query import SimpleStatement
+
 from app import app, cassandra_session
+
 from .models import Weather
 
 weather_input_topic = app.topic("weather-input", value_type=Weather)
@@ -17,29 +22,55 @@ weather_rejected_topic = app.topic("weather-rejected", value_type=Weather)
 
 logger = logging.getLogger(__name__)
 
+# temperature range above/below which we reject records
+MIN_C = -88.0
+MAX_C = 55.0
+
+
+def generate_weather_insert_statement(weather):
+    query = f'INSERT INTO demo.weather (' \
+        f'id, occurred_at, lat, long, temperature_c'\
+        f') VALUES (' \
+        f'{weather.id}, {weather.occurred_at}, ' \
+        f'{weather.lat}, {weather.long}, ' \
+        f'{weather.temperature_c}' \
+        f')'
+    return query
+
 
 def get_current_time_utc():
     epoch = datetime.datetime.utcfromtimestamp(0)
     return math.floor((datetime.datetime.utcnow() - epoch).total_seconds() * 1000.0)
 
 
+@app.page('/count/')
+async def get_count(self, request):
+    # note: this is not advisable to do in production but is a useful example
+    query = "SELECT count(*) from demo.weather;"
+
+    result = await cassandra_session.execute_future(
+        SimpleStatement(query, consistency_level=ConsistencyLevel.ONE)
+    )
+
+    return self.json({
+        'count': result[0][0],
+    })
+
+
 @app.agent(weather_input_topic)
 async def verify_and_store(weather_stream):
     async for weather in weather_stream:
-        accepted = -88.0 <= weather.temperature_c <= 55.0
+        accepted = MIN_C < weather.temperature_c < MAX_C
+
         logger.info(f"Verify: Weather (accepted: {accepted}): {weather}")
 
         event = current_event()
         if accepted:
-            query = \
-                f'INSERT INTO demo.weather (id, occurred_at, lat, long, temperature_c) ' \
-                f'VALUES (' \
-                f'{weather.id}, {weather.occurred_at}, ' \
-                f'{weather.lat}, {weather.long}, ' \
-                f'{weather.temperature_c}' \
-                f')'
+            query = generate_weather_insert_statement(weather)
 
-            cassandra_future = cassandra_session.execute_future(query)
+            cassandra_future = cassandra_session.execute_future(
+                SimpleStatement(query, consistency_level=ConsistencyLevel.ONE)
+            )
             event_forward_future = event.forward(weather_accepted_topic)
 
             await asyncio.gather(cassandra_future, event_forward_future)
@@ -47,9 +78,14 @@ async def verify_and_store(weather_stream):
             await event.forward(weather_rejected_topic)
 
 
+# emit messages_per_batch messages every 3 seconds
 @app.timer(interval=3)
 async def producer():
-    for i in range(0, 10):
+    messages_per_batch = 5
+
+    logger.info(f"Processing batch of {messages_per_batch}")
+
+    for i in range(messages_per_batch):
         message = {
             "id": str(uuid.uuid4()),
             "occurred_at": get_current_time_utc(),
@@ -59,7 +95,7 @@ async def producer():
         }
 
         await weather_input_topic.send(
-            key="faust",
+            key=str(uuid.uuid4()),
             value=json.dumps(message).encode('ascii')
         )
 
